c714121ae56e0b1b6fdbf0caa8dd8631c162ed9f
Sergej Tissen <sergej.tissen@gmail.com>
2025-12-09 09:22:49 +0100

    Revert "Lebensthema Coach Website - Komplette Implementierung"
    This reverts commit 78d43b569415cc9c085da28dc40db026702cafda.

--- app/coaching/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function CoachingPage() {
-  redirect("https://calendly.com/lebensthema/coach");
-}
-
--- app/podcast/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function PodcastPage() {
-  redirect("https://creators.spotify.com/pod/profile/sergej-tissen2/");
-}
-
